91885d141667a664370b1d79e9cabffb804b5c07
kannanATexcelforte <kannan@excelforte.com>
2022-09-10 17:39:38 +0530

    10.09.2022
    modal component and tabs learning has completed

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit } from '@angular/core';
 import { ModalService } from 'src/app/services/modal.service';
 
 @Component({
@@ -9,11 +9,21 @@ import { ModalService } from 'src/app/services/modal.service';
 })
 export class ModalComponent implements OnInit {
 
-  constructor(public modal: ModalService) {
+  @Input() modalID = '';
 
+  constructor(
+    public modal: ModalService,
+    public el: ElementRef
+  ) {
+    
   }
 
   ngOnInit(): void {
+    document.body.appendChild(this.el.nativeElement);
+  }
+
+  closeModal() {
+    this.modal.toggleModal(this.modalID);
   }
 
 }
