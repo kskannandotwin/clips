3af68b7a96bda56ec610af3ab79bfe82d8f5f649
kannanATexcelforte <kannan@excelforte.com>
2022-09-13 15:56:24 +0530

    13.09.2022
    upto chapter 7 completed

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -28,7 +28,15 @@ export class RegisterComponent {
   confirm_password = new FormControl('', [
     Validators.required
   ]);
-  phoneNumber = new FormControl('');
+  phoneNumber = new FormControl('', [
+    Validators.required,
+    Validators.minLength(13),
+    Validators.maxLength(13)
+  ]);
+
+  showAlert = false;
+  alertMsg = 'Please wait!. Your account is being created!';
+  alertColor = 'blue';
 
   registerForm = new FormGroup({
     name: this.name,
@@ -39,4 +47,10 @@ export class RegisterComponent {
     phoneNumber: this.phoneNumber
 
   });
+
+  register() {
+    this.showAlert = true;
+    this.alertMsg = 'Please wait!. Your account is being created!';
+    this.alertColor = 'blue';
+  }
 }
