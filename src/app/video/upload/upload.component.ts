bf355fdcd912cc79d3580504dd43c9f8f6144913
kannanATexcelforte <kannan@excelforte.com>
2022-09-21 15:20:20 +0530

    21.09.2022
    chapter 13 is in progress

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -7,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UploadComponent implements OnInit {
 
+  isDragover = false;
+  file: File | null = null;
+  nextStep = false;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  storeFile($event: Event) {
+    this.isDragover = false;
+
+    this.file = ($event as DragEvent).dataTransfer?.files.item(0) ?? null;
+
+    if(!this.file || this.file.type !== 'video/mp4') {
+      return;
+    }
+
+    this.nextStep = true;
+    
   }
 
 }
