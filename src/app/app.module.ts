26908af0747399b6ea0e38a5fac9ebd6de72d365
kannanATexcelforte <kannan@excelforte.com>
2023-10-09 16:02:36 +0530

    09.10.2023-namc
    completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { UserModule } from './user/user.module';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
