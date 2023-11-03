a1ec8ef0f10bad4256ae9b9bd841280929ec8319
kannanATexcelforte <kannan@excelforte.com>
2023-11-03 16:04:05 +0530

    03.11.2023-saat
    completed

--- src/app/shared/tab/tab.component.ts
+++ src/app/shared/tab/tab.component.ts
@@ -8,4 +8,5 @@ import { Component, Input } from '@angular/core';
 export class TabComponent {
 
   @Input() tabTitle = '';
+  @Input() active = false;
 }
--- src/app/shared/tabs-container/tabs-container.component.ts
+++ src/app/shared/tabs-container/tabs-container.component.ts
@@ -11,7 +11,18 @@ export class TabsContainerComponent {
   @ContentChildren(TabComponent) tabs?: QueryList<TabComponent> = new QueryList();
 
   ngAfterContentInit() {
-    // this.tabs
-    console.log(this.tabs);
+    const activeTabs = this.tabs?.filter(tab => tab.active);
+    if(!activeTabs || activeTabs.length === 0) {
+      this.selectTab(this.tabs!.first);
+    }
   }
+
+  selectTab(tab: TabComponent) {
+    this.tabs?.forEach(tab => {
+      tab.active = false;
+    });
+
+    tab.active = true;
+  }
+  
 }
