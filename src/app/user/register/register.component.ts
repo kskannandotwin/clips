2ae8fa96b739a260e5f28d326d8a6ba1e5863f50
kannanATexcelforte <kannan@excelforte.com>
2023-11-28 16:08:09 +0530

    28.11.2023-daac
    completed

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -34,6 +34,10 @@ export class RegisterComponent {
     Validators.maxLength(13)
   ]);
 
+  showAlert = false;
+  alertMsg = 'Please wait! Your account is being created.';
+  alertColor = 'blue';
+
   registerForm = new FormGroup({
     name: this.name,
     email: this.email,
@@ -44,6 +48,8 @@ export class RegisterComponent {
   });
 
   register() {
-    console.log('register called');
+    this.showAlert = true;
+    this.alertMsg = 'Please wait! Your account is being created.';
+    this.alertColor = 'blue';
   }
 }
