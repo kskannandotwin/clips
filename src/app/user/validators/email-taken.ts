b264c660ec9aabecdbb9fec767176eafc2213fe6
kannanATexcelforte <kannan@excelforte.com>
2023-12-14 19:28:39 +0530

    14.12.2023-vue
    completed

--- src/app/user/validators/email-taken.ts
+++ src/app/user/validators/email-taken.ts
@@ -1,13 +1,14 @@
 import { AngularFireAuth } from "@angular/fire/compat/auth";
 import { Injectable } from "@angular/core";
 import { AbstractControl, AsyncValidator, ValidationErrors } from "@angular/forms";
-import { Observable } from "rxjs";
 
 @Injectable()
 export class EmailTaken implements AsyncValidator {
     constructor(private auth: AngularFireAuth) { }
 
-    validate() {
-        
+    validate(control: AbstractControl): Promise<ValidationErrors | null> {
+        return this.auth.fetchSignInMethodsForEmail(control.value).then(
+            response => response.length ? { EmailTaken: true } : null
+        );
     }
 }
