592d7238b3bf1fad5a45d097bcece739c7bec1a6
kannanATexcelforte <kannan@excelforte.com>
2023-12-14 14:30:55 +0530

    14.12.2023-vp
    completed

--- src/app/user/validators/register-validators.ts
+++ src/app/user/validators/register-validators.ts
@@ -1,6 +1,17 @@
-export class RegisterValidators {
-    static match() {
+import { ValidationErrors, AbstractControl } from "@angular/forms";
 
+export class RegisterValidators {
+    static match(group: AbstractControl): ValidationErrors | null {
+        const control = group.get('password');
+        const matchingControl = group.get('confirm_password');
+
+        if (!control || !matchingControl) {
+            return { controlNotFound: false };
+        }
+
+        const error = control.value === matchingControl.value ? null : { noMatch: true };
+
+        return error;
     }
 }
 
