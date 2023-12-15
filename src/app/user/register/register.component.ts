e8bca945e08576d056fffbee5007d712363c5079
kannanATexcelforte <kannan@excelforte.com>
2023-12-15 10:35:33 +0530

    15.12.2023-ftv
    completed but this lecture not working for email validator and solutions to be findout

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -3,6 +3,7 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { AuthService } from 'src/app/services/auth.service';
 import IUser from 'src/app/models/user.model';
 import { RegisterValidators } from '../validators/register-validators';
+import { EmailTaken } from '../validators/email-taken';
 
 @Component({
   selector: 'app-register',
@@ -12,7 +13,8 @@ import { RegisterValidators } from '../validators/register-validators';
 export class RegisterComponent {
 
   constructor(
-    private auth: AuthService
+    private auth: AuthService,
+    private emailTaken: EmailTaken
   ) { }
 
   inSubmission = false;
@@ -24,7 +26,7 @@ export class RegisterComponent {
   email = new FormControl('', [
     Validators.required,
     Validators.email
-  ]);
+  ], [this.emailTaken.validate]);
   age = new FormControl<number | null>(null, [
     Validators.required,
     Validators.min(18),
