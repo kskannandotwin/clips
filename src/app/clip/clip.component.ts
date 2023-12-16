39744dde24483edc4fe324155bb051fd59b8a26a
kannanATexcelforte <kannan@excelforte.com>
2023-12-16 11:47:11 +0530

    16.12.2023-rp
    completed

---
+++ src/app/clip/clip.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-clip',
+  templateUrl: './clip.component.html',
+  styleUrls: ['./clip.component.css']
+})
+export class ClipComponent implements OnInit {
+
+  id = '';
+
+  constructor(public route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.id = this.route.snapshot.params.id;
+  }
+}
