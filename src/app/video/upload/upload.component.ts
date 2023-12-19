5f08207d04882085e642ee8ed0a31ffa7bba0222
kannanATexcelforte <kannan@excelforte.com>
2023-12-19 16:53:26 +0530

    19.12.2023-hf
    completed

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -8,9 +8,16 @@ import { Component } from '@angular/core';
 export class UploadComponent {
 
   isDragOver = false;
+  file: File | null = null;
 
   storeFile($event: Event) {
     this.isDragOver = false;
+    this.file = ($event as DragEvent).dataTransfer?.files.item(0) ?? null;
+    if (!this.file || this.file.type !== 'video/mp4') {
+      return
+    }
+
+    console.log(this.file);
   }
 
 }
