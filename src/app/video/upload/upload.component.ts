87e4da2dafb87902c453884e89fc036af84d00cd
kannanATexcelforte <kannan@excelforte.com>
2023-12-20 10:56:21 +0530

    20.12.2023-msf
    completed

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -9,15 +9,17 @@ export class UploadComponent {
 
   isDragOver = false;
   file: File | null = null;
+  nextStep = false;
 
   storeFile($event: Event) {
     this.isDragOver = false;
+
     this.file = ($event as DragEvent).dataTransfer?.files.item(0) ?? null;
     if (!this.file || this.file.type !== 'video/mp4') {
       return
     }
 
-    console.log(this.file);
+    this.nextStep = true;
   }
 
 }
