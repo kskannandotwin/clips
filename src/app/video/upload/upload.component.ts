8250cc44161e9063f7d21247f7dfe011aadd18e9
kannanATexcelforte <kannan@excelforte.com>
2023-12-22 15:15:56 +0530

    22.12.2023-stfd
    completed

--- src/app/video/upload/upload.component.ts
+++ src/app/video/upload/upload.component.ts
@@ -2,7 +2,9 @@ import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { AngularFireStorage } from '@angular/fire/compat/storage';
 import { v4 as uuid } from 'uuid';
-import { last } from 'rxjs/operators';
+import { last, switchMap } from 'rxjs/operators';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import firebase from 'firebase/compat/app';
 
 @Component({
   selector: 'app-upload',
@@ -20,6 +22,7 @@ export class UploadComponent {
   inSubmission = false;
   percentage = 0;
   showPercentage = false;
+  user: firebase.User | null = null;
 
   title = new FormControl('', [
     Validators.required,
@@ -29,7 +32,12 @@ export class UploadComponent {
     title: this.title
   });
 
-  constructor(private storage: AngularFireStorage) {}
+  constructor(
+    private storage: AngularFireStorage,
+    private auth: AngularFireAuth
+    ) {
+      auth.user.subscribe(user => this.user = user);
+    }
 
   storeFile($event: Event) {
     this.isDragOver = false;
@@ -56,15 +64,25 @@ export class UploadComponent {
     const clipPath = `clips/${clipFileName}.mp4`;
 
     const task = this.storage.upload(clipPath, this.file);
+    const clipRef = this.storage.ref(clipPath);
 
     task.percentageChanges().subscribe(progress => {
       this.percentage = progress as number / 100;
     });
 
     task.snapshotChanges().pipe(
-      last()
+      last(),
+      switchMap(() => clipRef.getDownloadURL())
     ).subscribe({
-      next: (snapshot) => {
+      next: (url) => {
+        const clip = {
+          uid: this.user?.uid,
+          displayName: this.user?.displayName,
+          title: this.title.value,
+          fileName: `${clipFileName}.mp4`,
+          url
+        }
+        console.log(clip);
         this.alertColor = 'green';
         this.alertMsg = 'Success! Your clip is now ready to share with the World';
         this.showPercentage = false;
