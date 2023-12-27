8f8ce49bc134a99fcc5b59000418a5b441d72e47
kannanATexcelforte <kannan@excelforte.com>
2023-12-27 12:55:02 +0530

    27.12.2023-qtd
    completed

--- src/app/video/manage/manage.component.ts
+++ src/app/video/manage/manage.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
+import { ClipService } from 'src/app/services/clip.service';
 
 @Component({
   selector: 'app-manage',
@@ -10,12 +11,17 @@ export class ManageComponent implements OnInit {
 
   videoOrder = '1';
 
-  constructor(private router: Router, private route: ActivatedRoute) {}
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private clipService: ClipService
+    ) {}
 
   ngOnInit() {
     this.route.queryParams.subscribe((params: Params) => {
       this.videoOrder = params['sort'] === '2' ? params['sort'] : 1;
     });
+    this.clipService.getUserClips().subscribe(console.log);
   }
 
   sort(event: Event) {
