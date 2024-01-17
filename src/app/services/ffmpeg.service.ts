f82c25678ca95c7b244d7081e301936fbff65f98
kannanATexcelforte <kannan@excelforte.com>
2024-01-17 11:20:52 +0530

    17.01.2024-uab
    completed

--- src/app/services/ffmpeg.service.ts
+++ src/app/services/ffmpeg.service.ts
@@ -70,4 +70,11 @@ export class FfmpegService {
 
     return screenshots;
   }
+
+  async blobFromURL(url: string) {
+    const response = await fetch(url);
+    const blob = await response.blob();
+
+    return blob;
+  }
 }
